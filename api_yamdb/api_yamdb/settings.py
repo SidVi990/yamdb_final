48e80086e300ccef765f8b4f5c520c2016bcbcfd
Евгений Малый <sidvicious990@gmail.com>
2023-03-13 22:44:03 +0700

    Review: first edits

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -59,7 +59,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
-
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
